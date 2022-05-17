f6fae421c5e7ad588f692f313911a831608c55a9
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-05-17 16:36:12 +0100

    Add files

---
+++ include/Ishiko/YAML/YAMLPushParser.hpp
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/yaml/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CPP_YAML_YAMLPUSHPARSER_HPP_
+#define _ISHIKO_CPP_YAML_YAMLPUSHPARSER_HPP_
+
+#endif
---
+++ src/YAMLPushParser.cpp
@@ -0,0 +1,7 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/yaml/blob/main/LICENSE.txt
+*/
+
+#include "YAMLPushParser.hpp"
